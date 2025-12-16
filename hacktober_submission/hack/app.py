f894f02f42dafae1367f99bcd2e14b95f282cdc8
MAANASA-KOTTE <maanasa.kotte@gmail.com>
2025-12-16 13:10:17 +0530

    Prepare for deployment

--- hacktober_submission/hack/app.py
+++ hacktober_submission/hack/app.py
@@ -127,4 +127,5 @@ def reset():
     return jsonify({"message": "All outfits cleared."})
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+
